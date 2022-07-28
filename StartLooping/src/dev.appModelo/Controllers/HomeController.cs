8db59b72a734e4d1c9fb3cfacdec3344f3cfe8a1
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-07-28 14:12:31 -0300

    TagHelper

---
+++ StartLooping/src/dev.appModelo/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace dev.appModelo.Controllers
+{
+    public class HomeController: Controller
+    {
+        [Route("")]
+        [Route("StartLooping")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

c2378bab453b0864fb8f49f6c3ecbebcbc0cfc02
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-07-28 13:02:24 -0300

    Areas

---
+++ StartLooping/src/dev.appModelo/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace dev.appModelo.Controllers
+{
+    [Route("")]
+    [Route("StartLoping")]
+    public class HomeController: Controller
+    {   
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

79cf4b5be1c26e3df479317de9d6397821b41955
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-07-29 09:45:11 -0300

    Busca-View

---
+++ ASP.NET.3.2-NewProject/StartLooping/src/dev.appModelo/Areas/Produtos/Controllers/CadastroController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dev.appModelo.Areas.Produtos.Controllers
+{
+    [Area("Produtos")]
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Produtos/Busca")]
+        public IActionResult Busca()
+        {
+            return View();
+        }
+    }
+}
